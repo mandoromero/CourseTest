afab4bf99c1f7b6595ee42ccedfac0ca5fbfda9b
Armando Romero <162987396+mandoromero@users.noreply.github.com>
2024-10-23 02:56:36 +0000

    "1st Challenge"

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// /* eslint-disable */
+// import "bootstrap";
+// import "./style.css";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// window.onload = function() {
+//   var submenus = document.querySelectorAll("ul li ul");
+
+//   for (var i = 0; i < submenus.length; i++) {
+//     submenus[i].classname = "hide-menu";
+//   }
+
+//   var menulinks = document.querySelectorAll("menulinks");
+
+//   for (var i = 0; i < menulinks.length; i++) {
+//     menulinks[i].addEventListener("click", function(e) {
+//       e.preventDefault();
+//       var thisMenu = this;
+//       alert(thisMenu.innerHTML);
+//     });
+//   }
+// };
