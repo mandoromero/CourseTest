2fa9f2b479325a7cc1904abff4310b1dcfbf41b4
Armando Romero <162987396+mandoromero@users.noreply.github.com>
2024-10-24 03:48:23 +0000

    coursera project

--- src/app.js
+++ src/app.js
@@ -1,31 +1,31 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// /* eslint-disable */
+// import "bootstrap";
+// import "./style.css";
 
-window.onload = function() {
-  "use strict";
-  const submenus = document.querySelectorAll("ul li ul");
+// window.onload = function() {
+//   "use strict";
+//   const submenus = document.querySelectorAll("ul li ul");
 
-  const hideSubmenus = () => {
-    for (let i = 0; i < submenus.length; i++) {
-      submenus[i].className = "hide-menu"; // No dot here
-    }
-  };
+//   const hideSubmenus = () => {
+//     for (let i = 0; i < submenus.length; i++) {
+//       submenus[i].className = "hide-menu"; // No dot here
+//     }
+//   };
 
-  hideSubmenus();
+//   hideSubmenus();
 
-  const menulinks = document.querySelectorAll(".menulink");
-  for (let i = 0; i < menulinks.length; i++) {
-    menulinks[i].addEventListener("click", function(event) {
-      event.preventDefault();
+//   const menulinks = document.querySelectorAll(".menulink");
+//   for (let i = 0; i < menulinks.length; i++) {
+//     menulinks[i].addEventListener("click", function(event) {
+//       event.preventDefault();
 
-      const thisMenu = this.parentNode.querySelector("ul");
-      if (thisMenu.classList.contains("hide-menu")) {
-        hideSubmenus();
-        thisMenu.className = "show-menu";
-      } else {
-        thisMenu.className = "hide-menu";
-      }
-    });
-  }
-};
+//       const thisMenu = this.parentNode.querySelector("ul");
+//       if (thisMenu.classList.contains("hide-menu")) {
+//         hideSubmenus();
+//         thisMenu.className = "show-menu";
+//       } else {
+//         thisMenu.className = "hide-menu";
+//       }
+//     });
+//   }
+// };
